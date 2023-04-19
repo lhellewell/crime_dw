56756b8ff797c78393c1f06a8036e820be6c5f69
Lewis-Hellewell <imlewishellewell@gmail.com>
2023-04-19 12:30:53 +0800

    feat: bulk insert query

--- Insert Query.sql
+++ Insert Query.sql
@@ -38,3 +38,23 @@ WITH (
     --KEEPIDENTITY,
     TABLOCK
 );
+
+BULK INSERT [dbo].[DimDate] FROM 'D:\UNI2023\Data Warehousing\Project 1\ETL and Data\DimCity.csv'
+WITH (
+    CHECK_CONSTRAINTS,
+    DATAFILETYPE='char',
+    FIELDTERMINATOR=',',
+    ROWTERMINATOR='\n',
+    --KEEPIDENTITY,
+    TABLOCK
+);
+
+BULK INSERT [dbo].[DimPatrolBeat] FROM 'D:\UNI2023\Data Warehousing\Project 1\ETL and Data\DimPatrolBeat.csv'
+WITH (
+    CHECK_CONSTRAINTS,
+    DATAFILETYPE='char',
+    FIELDTERMINATOR=',',
+    ROWTERMINATOR='\n',
+    --KEEPIDENTITY,
+    TABLOCK
+);
