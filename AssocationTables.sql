58748127dd78f2587c76ec7bbc659e533bda82e6
Lewis-Hellewell <imlewishellewell@gmail.com>
2023-04-23 13:42:11 +0800

    feat: crime num added to case

---
+++ AssocationTables.sql
@@ -0,0 +1,68 @@
+USE master;
+GO
+
+Use Crime
+Go
+
+PRINT '';
+PRINT '*** Dropping Association Tables if they already exist';
+GO
+
+IF OBJECT_ID('CaseTable', 'U') IS NOT NULL
+DROP TABLE CaseTable;
+
+IF OBJECT_ID('NestedTable', 'U') IS NOT NULL
+DROP TABLE NestedTable;
+GO
+
+PRINT '';
+PRINT '*** Creating Table CaseTable';
+GO
+
+Create table CaseTable
+(
+CrimeNumber bigint primary key ,
+CaseKey int,
+Date varchar(20) not null,
+Neighborhood varchar(50) not null,
+)
+Go
+
+PRINT '';
+PRINT '*** Creating Table NestedTable';
+GO
+
+Create table NestedTable
+(
+CaseKey int,
+CrimeType varchar(50) not null,
+)
+Go
+
+PRINT '';
+PRINT '*** Inserting CaseTable.csv';
+GO
+
+BULK INSERT [dbo].[CaseTable] FROM 'D:\UNI2023\Data Warehousing\Project 1\ETL and Data\CaseTable.csv'
+WITH (
+	FIRSTROW=2,
+    DATAFILETYPE='char',
+    FIELDTERMINATOR=',',
+    ROWTERMINATOR='\n',
+    --KEEPIDENTITY,
+    TABLOCK
+);
+
+PRINT '';
+PRINT '*** Inserting NestedTable.csv';
+GO
+
+BULK INSERT [dbo].[NestedTable] FROM 'D:\UNI2023\Data Warehousing\Project 1\ETL and Data\NestedTable.csv'
+WITH (
+	FIRSTROW=2,
+    DATAFILETYPE='char',
+    FIELDTERMINATOR=',',
+    ROWTERMINATOR='\n',
+    --KEEPIDENTITY,
+    TABLOCK
+);
--- ETL and Data/assocation.py
+++ ETL and Data/assocation.py
@@ -10,8 +10,9 @@ df = pd.merge(df, DimDate, on="DateKey")
 df = pd.merge(df, DimLocation, on="LocationKey")
 df = pd.merge(df, DimCrimeType, on="CrimeTypeKey")
 
+
 # Drop unnecessary columns
-df = df[['Neighborhood', 'Date', 'CrimeType']]
+df = df[['CrimeNumber', 'Neighborhood', 'Date', 'CrimeType']].drop_duplicates(subset=['CrimeNumber'])
 
 df = df.sort_values(by=['Date', 'Neighborhood'])
 
@@ -22,12 +23,9 @@ case_key.insert(0, 'CaseKey', range(len(case_key)))
 # Merging to main df gain case key
 df = pd.merge(df, case_key, on=['Neighborhood', 'Date'])
 
-# Re-order columns
-df = df[['CaseKey', 'Date', 'Neighborhood', 'CrimeType']]
-
 # Creating the tables
 nested_table = df[['CaseKey', 'CrimeType']].drop_duplicates()
-case_table = df[['CaseKey', 'Date', 'Neighborhood']].drop_duplicates()
+case_table = df[['CrimeNumber', 'CaseKey', 'Date', 'Neighborhood']].drop_duplicates()
 
 #print(nested_table)
 #print(case_table)
